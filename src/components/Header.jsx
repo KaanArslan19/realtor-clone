5ee163d7141837652ea6f189283a513f4966e9d0
Kaan Arslan <kaanarslan35@gmail.com>
2022-10-30 16:43:10 +0300

    create sign in, sign up and forgot password pages and oath compenent git push cls

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,13 +10,13 @@ export default function Header() {
     return true
   }
   return (
-    <div>
+    <div  className="bg-white border-b shadow-sm sticky top-0 z-50">
     <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
       <div>
       <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="logo" className="h-5 cursor-pointer"  />
       </div>
 
-      <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+      <div>
         <ul className='flex space-x-10'>
           <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent  cursor-pointer ${pathMatchRoute("/") && "text-black border-b-red-500"}`}
           onClick={() => navigate("/") }
