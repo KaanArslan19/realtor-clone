f270da9f70ae19b8f410a85cc45d46698aaf5643
Kaan Arslan <kaanarslan35@gmail.com>
2022-11-16 11:52:53 +0300

    delete functionality added to the myListing section

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -10,6 +10,7 @@ import {
   query,
   where,
   getDocs,
+  deleteDoc,
 } from "firebase/firestore";
 import { db } from "../firebase";
 import { FcHome } from "react-icons/fc";
@@ -80,85 +81,102 @@ export default function Profile() {
     }
     fetchUserListings();
   }, [auth.currentUser.uid]);
+
+  async function onDelete(listingID) {
+    if (window.confirm("Are you sure you want to delete ?")) {
+      await deleteDoc(doc(db, "listings", listingID));
+      const updatedListings = listings.filter(
+        (listing) => listing.id !== listingID
+      ); // keep every ID except the deleted one
+      setListings(updatedListings);
+      toast.success("Item deleted successfully");
+    }
+  }
+
+  function onEdit(listingID) {
+    navigate(`/edit-listing/${listingID}`);
+  }
   return (
     <>
-    <section className="max-w-6xl mx-auto flex justify-center flex-col items-center">
-      <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
-      <div className="w-full md:w-[50%] mt-6 px-3">
-        <form>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            disabled={!changeDetail}
-            onChange={onChange}
-            className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
-              changeDetail && "bg-red-200 focus:bg-red-200"
-            }`}
-          />
+      <section className="max-w-6xl mx-auto flex justify-center flex-col items-center">
+        <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
+        <div className="w-full md:w-[50%] mt-6 px-3">
+          <form>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              disabled={!changeDetail}
+              onChange={onChange}
+              className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
+                changeDetail && "bg-red-200 focus:bg-red-200"
+              }`}
+            />
 
-          <input
-            type="email"
-            id="email"
-            value={email}
-            disabled
-            className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
-          />
+            <input
+              type="email"
+              id="email"
+              value={email}
+              disabled
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+            />
 
-          <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
-            <p className="flex items-center">
-              Do you want to change your name ?
-              <span
-                onClick={() => {
-                  //have to call it one time otherwise it gives infinite loop
-                  changeDetail && onSubmit();
-                  setchangeDetail((prevState) => !prevState);
-                }}
-                className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+              <p className="flex items-center">
+                Do you want to change your name ?
+                <span
+                  onClick={() => {
+                    //have to call it one time otherwise it gives infinite loop
+                    changeDetail && onSubmit();
+                    setchangeDetail((prevState) => !prevState);
+                  }}
+                  className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
+                >
+                  {changeDetail ? "Apply change" : "Edit"}
+                </span>
+              </p>
+              <p
+                onClick={onLogOut}
+                className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer"
               >
-                {changeDetail ? "Apply change" : "Edit"}
-              </span>
-            </p>
-            <p
-              onClick={onLogOut}
-              className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer"
-            >
-              Sign out
-            </p>
-          </div>
-        </form>
-        <button
-          type="submit"
-          className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800 "
-        >
-          <Link
-            to="/create-listing"
-            className="flex justify-center items-center "
+                Sign out
+              </p>
+            </div>
+          </form>
+          <button
+            type="submit"
+            className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800 "
           >
-            <FcHome className="mr-2 text-3xl bg-red-200 rounded-full p-1 border-2" />
-            Sell or Rent your Home
-          </Link>
-        </button>
-      </div>
-      
-    </section>
-    <div className="max-w-6xl px-3 mt-6 mx-auto">
+            <Link
+              to="/create-listing"
+              className="flex justify-center items-center "
+            >
+              <FcHome className="mr-2 text-3xl bg-red-200 rounded-full p-1 border-2" />
+              Sell or Rent your Home
+            </Link>
+          </button>
+        </div>
+      </section>
+      <div className="max-w-6xl px-3 mt-6 mx-auto">
         {!loading && listings.length > 0 && (
           <>
-            <h2 className="text-2xl text-center font-semibold mb-6 ">My listings</h2>
+            <h2 className="text-2xl text-center font-semibold mb-6 ">
+              My listings
+            </h2>
             <ul className="sm: grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6">
               {listings.map((listing) => (
                 <ListingItem
                   key={listing.id}
                   id={listing.id}
                   listing={listing.data}
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
                 />
               ))}
             </ul>
           </>
         )}
       </div>
-</>
-
+    </>
   );
 }
