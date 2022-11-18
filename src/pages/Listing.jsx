61de4fb80fe4cc728166befecfca6daee8c3f13e
Kaan Arslan <kaanarslan35@gmail.com>
2022-11-18 21:41:33 +0300

    share&copy functionality added

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -12,11 +12,13 @@ import SwiperCore, {
 } from "swiper";
 import "swiper/css/bundle";
 import "swiper/css/bundle";
+import  {BiLinkAlt} from "react-icons/bi"
 
 export default function Listing() {
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const[shareLinkCopied, setShareLinkCopied] = useState(false)
   SwiperCore.use([Autoplay, Navigation, Pagination]);
   useEffect(() => {
     async function fetchListing() {
@@ -54,6 +56,19 @@ export default function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-6 h-6 flex justify-center items-center"
+      onClick={() =>{
+        navigator.clipboard.writeText(window.location.href);
+        setShareLinkCopied(true);
+        setTimeout(() => {
+          setShareLinkCopied(false);
+        },2000)
+      }}>
+        <BiLinkAlt className="text-lg text-slate-700"/>
+      </div>
+      {shareLinkCopied && (
+        <p className="px-1 fixed top-[18%] right-[5%] font-semibold border-2 border-gray-400 rounded-l bg-white z-10">Link Copied</p>
+      )}
     </main>
   );
 }
