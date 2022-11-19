9dac3f94fe53cacee2418e1dccb26964fb4e3f2e
Kaan Arslan <kaanarslan35@gmail.com>
2022-11-19 15:32:06 +0300

    The landlord contact component created

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -13,9 +13,19 @@ import SwiperCore, {
 import "swiper/css/bundle";
 import "swiper/css/bundle";
 import { BiLinkAlt } from "react-icons/bi";
-import { FaMapMarkerAlt, FaBed, FaBath, FaParking, FaChair } from "react-icons/fa";
+import {
+  FaMapMarkerAlt,
+  FaBed,
+  FaBath,
+  FaParking,
+  FaChair,
+} from "react-icons/fa";
+import { getAuth } from "firebase/auth";
+import Contact from "../components/Contact";
 
 export default function Listing() {
+  const [contactLandlord, setContactLandlord] = useState(false);
+  const auth = getAuth();
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -76,7 +86,7 @@ export default function Listing() {
       )}
 
       <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
-        <div className="w-full h-[200px] lg-[400px]">
+        <div className="w-full lg-[400px]">
           <p className="text-2xl font-bold mb-3 text-blue-900 ">
             {listing.name} - $
             {listing.offer
@@ -108,26 +118,37 @@ export default function Listing() {
             <span className="font-semibold">Description - </span>
             {listing.description}
           </p>
-          <ul className="flex items-center space-x-2 sm:space-x-10 text-sm font-semibold">
+          <ul className="flex items-center space-x-2 sm:space-x-10 text-sm font-semibold mb-6">
             <li className="flex items-center whitespace-nowrap">
-              <FaBed className="text-lg mr-1"/>
-              {+listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed" }
+              <FaBed className="text-lg mr-1" />
+              {+listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
             </li>
 
             <li className="flex items-center whitespace-nowrap">
-              <FaBath className="text-lg mr-1"/>
-              {+listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath" }
+              <FaBath className="text-lg mr-1" />
+              {+listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath"}
             </li>
 
             <li className="flex items-center whitespace-nowrap">
-              <FaParking className="text-lg mr-1"/>
+              <FaParking className="text-lg mr-1" />
               {+listing.parking ? "parking spot" : "No parking"}
             </li>
             <li className="flex items-center whitespace-nowrap">
-              <FaChair className="text-lg mr-1"/>
+              <FaChair className="text-lg mr-1" />
               {+listing.furnished ? "Furnished" : "Not furnished"}
             </li>
           </ul>
+          {listing.userRef == auth.currentUser?.uid && !contactLandlord && (
+            <div className="mt-6">
+              <button onClick={() => setContactLandlord(true)} className="px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-150 ease-in-out">
+                Contact the Landlord
+              </button>
+            </div>
+          )}
+          {contactLandlord && (
+            <Contact userRef={listing.userRef} listing={listing}/>
+          )}
+          {/* we protect it in case the page loaded faster*/}
         </div>
         <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
       </div>
